e05d158f548b7b8684ff80d7a9170f02c6792959
htkien <htkien2511@gmail.com>
2020-06-16 10:19:12 +0700

    add outlet of collectionviewcell

--- OpenWeather2/Application/Weather/WeatherCollectionViewCell.swift
+++ OpenWeather2/Application/Weather/WeatherCollectionViewCell.swift
@@ -9,8 +9,19 @@
 import UIKit
 
 class WeatherCollectionViewCell: UICollectionViewCell {
-    
-  @IBOutlet weak var weatherGerenal: UIView!
+  
+  // MARK: - Outlet
+  @IBOutlet weak var cityLabel: UILabel!
+  @IBOutlet weak var iconImageView: UIImageView!
+  @IBOutlet weak var temperatureLabel: UILabel!
+  @IBOutlet weak var weatherDescriptionLabel: UILabel!
+  
+  // MARK: - Outlet Detail Weather
+  @IBOutlet var tempDetailArray: Array<UILabel>?
+  @IBOutlet var iconDetailArray: Array<UIImageView>?
+  @IBOutlet var dayDetailArray: Array<UILabel>?
+  
+  // MARK: - Action
   @IBAction func everyHourButtonTapped(_ sender: Any) {
     print("every hour tapped")
   }
