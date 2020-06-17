7efcb6b05073fd19d7fea3e29ae1e301ae9dfd3d
htkien <htkien2511@gmail.com>
2020-06-17 12:05:38 +0700

    set up page control

--- OpenWeather2/Application/Weather/WeatherViewController.swift
+++ OpenWeather2/Application/Weather/WeatherViewController.swift
@@ -15,6 +15,8 @@ class WeatherViewController: UIViewController {
   // MARK: - Outlet
   @IBOutlet weak var updatedDayLabel: UILabel!
   @IBOutlet weak var updatedTimeLabel: UILabel!
+  @IBOutlet weak var pageControl: UIPageControl!
+  @IBOutlet weak var collectionView: UICollectionView!
   
   // MARK: - Properties
   private var items: [DataStructs] = []
@@ -24,18 +26,70 @@ class WeatherViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    setUpPageControl()
+    setUpCollectionView()
   }
   
+  func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    changePageControl()
+  }
   
+  func setUpCollectionView() {
+    collectionView.isPagingEnabled = true
+    collectionView.showsHorizontalScrollIndicator = false
+  }
+  
+  func setUpPageControl() {
+    pageControl.numberOfPages = items.count < 1 ? 1 : items.count
+  }
+  
+  func changePageControl() {
+    pageControl.currentPage = getCurrentIndex()
+  }
+  
+  private func getCurrentIndex() -> Int {
+    print(collectionView.contentOffset.x)
+    print(collectionView.frame.width)
+    return Int(collectionView.contentOffset.x / collectionView.frame.width)
+  }
   
   // MARK: - Action
   @IBAction func addCityButtonTapped(_ sender: Any) {
+    addCity() { (city) in
+      let dataManager = DataManager(baseURL: API.AuthenticatedBaseURL)
+      dataManager.weatherDataForLocation(city: city) { (data, error) in
+        if let _ = error {
+          print(error!)
+        }
+        else {
+          DispatchQueue.main.async {
+            self.items.append(data!)
+            self.setUpPageControl()
+            self.collectionView.reloadData()
+          }
+        }
+      }
+    }
+  }
+  
+  func addCity(completion: @escaping (_ city: String) -> ()) {
+    let alertController = UIAlertController(title: "Title", message: "", preferredStyle: .alert)
+    alertController.addAction(UIAlertAction(title: "Save", style: .default, handler: { alert -> Void in
+      let textField = alertController.textFields![0] as UITextField
+      completion(textField.text!)
+    }))
+    alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+    alertController.addTextField(configurationHandler: {(textField : UITextField!) -> Void in
+      textField.placeholder = "Search"
+    })
+    self.present(alertController, animated: true, completion: nil)
   }
 }
 
 // MARK: - Collection View Data Source
 extension WeatherViewController: UICollectionViewDataSource {
-  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+  func collectionView(_ collectionView: UICollectionView,
+                      numberOfItemsInSection section: Int) -> Int {
     // API khong kip tra ve nen items.count = 0 => failed
     return items.count < 1 ? 1 : items.count
   }
@@ -44,15 +98,25 @@ extension WeatherViewController: UICollectionViewDataSource {
                       cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier,
                                                   for: indexPath) as! WeatherCollectionViewCell
-    loadDataFromAPI(cell, indexPath: indexPath)
+    if items.count == 0 {
+      loadDataFromAPI(cell, indexPath: indexPath, city: "Hue")
+    }
+    else {
+      self.setUpWeather(cell, indexPath: indexPath)
+      self.setUpEveryWeather(cell, indexPath: indexPath)
+      self.setUpdatedTime()
+    }
+    
     cell.delegate = self
     return cell
   }
   
   // MARK: - Helper Method
-  func loadDataFromAPI(_ cell: WeatherCollectionViewCell, indexPath: IndexPath) {
+  func loadDataFromAPI(_ cell: WeatherCollectionViewCell,
+                       indexPath: IndexPath,
+                       city: String) {
     let dataManager = DataManager(baseURL: API.AuthenticatedBaseURL)
-    dataManager.weatherDataForLocation(city: "Hue") { (data, error) in
+    dataManager.weatherDataForLocation(city: city) { (data, error) in
       if let _ = error {
         print(error!)
       }
@@ -67,7 +131,8 @@ extension WeatherViewController: UICollectionViewDataSource {
     }
   }
   
-  func setUpWeather(_ cell: WeatherCollectionViewCell, indexPath: IndexPath) {
+  func setUpWeather(_ cell: WeatherCollectionViewCell,
+                    indexPath: IndexPath) {
     let currentIndex = HelperWeather.getLastedIndex(data: items[indexPath.item]) + 1
     
     cell.cityLabel.text = String("\(self.items[indexPath.item].city.name)")
@@ -76,7 +141,8 @@ extension WeatherViewController: UICollectionViewDataSource {
     cell.iconImageView.image = UIImage(named: self.items[indexPath.item].list[currentIndex].weather[0].icon)
   }
   
-  func setUpEveryWeather(_ cell: WeatherCollectionViewCell, indexPath: IndexPath) {
+  func setUpEveryWeather(_ cell: WeatherCollectionViewCell,
+                         indexPath: IndexPath) {
     let detailEveryHour = HelperWeather.getWeatherEveryHour(data: items[indexPath.item])
     let lastedIndex = HelperWeather.getLastedIndex(data: items[indexPath.item])
     for i in 0..<6 {
@@ -105,6 +171,12 @@ extension WeatherViewController: UICollectionViewDelegateFlowLayout {
                       layout collectionViewLayout: UICollectionViewLayout,
                       sizeForItemAt indexPath: IndexPath) -> CGSize {
     return collectionView.frame.size
+  }
+  
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+    return 0
   }
 }
 
