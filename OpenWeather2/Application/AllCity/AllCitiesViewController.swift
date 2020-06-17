0d5bdacea2be863edc59a95916b3e240c513ca6d
htkien <htkien2511@gmail.com>
2020-06-17 15:27:52 +0700

    setup all city view controller

---
+++ OpenWeather2/Application/AllCity/AllCitiesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AllCities.swift
+//  OpenWeather2
+//
+//  Created by Hoang Trong Kien on 6/17/20.
+//  Copyright © 2020 Hoang Trong Kien. All rights reserved.
+//
+
+import UIKit
+
+class AllCitiesViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
